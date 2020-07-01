cd1927739a4d19acb44ff204d66b86004426f0bb
nrrf <nrrojasfe@unal.edu.co>
2020-07-01 12:29:02 -0500

    routh by name in localhost

--- index.js
+++ index.js
@@ -8,25 +8,17 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+/*
 app.get('/hola', (req,res) => {
     res.send({message: 'Hola Mundo!'})
+})
+
+*/ 
+
+app.get('/hola/:name', (req,res) => {
+    res.send({message: `Hola ${req.params.name}!`})
 })
 
 app.listen(port, () => {
     console.log(`API REST corriendo en http://localhost:${port}`)
 })
-
-
-
-/*
-const http = require('http'); 
-const server = http.createServer(function(req,res){
-    //res.writeHead(200,{'content-type' : 'text/plain'});
-    //res.end("hola mundo");
-    
-    res.end('Hola mundo');
-});
-server.listen(3000,()=>{
-    console.log('server on port 3000')
-});
-*/

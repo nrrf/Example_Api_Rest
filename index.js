893fc3c053bb612e7eb92400bf7a76f7d1abbc1f
nrrf <nrrojasfe@unal.edu.co>
2020-07-02 15:32:04 -0500

    add get, post, put and delete

--- index.js
+++ index.js
@@ -14,10 +14,37 @@ app.get('/hola', (req,res) => {
 })
 
 */ 
-
+/*
 app.get('/hola/:name', (req,res) => {
     res.send({message: `Hola ${req.params.name}!`})
 })
+*/ 
+app.get('/api/product', (req,res) => {
+    res.send(200,{products:[]})   // segun http 200 es que es correcto
+})
+
+app.get('/api/product/:productId', (req,res) => {
+    
+})
+
+
+app.post('/api/product', (req,res) => {
+    console.log(req.body)
+    //res.send(200, {message : "El producto se ha recibido"})
+    //res.status(200).send({message : "El producto se ha recibido"})
+    res.status(404).send({message : "El producto no existe"})
+})
+
+
+app.put('/api/product/:productId', (req,res) => {
+       // segun http 200 es que es correcto
+})
+
+app.delete('/api/product/:productId', (req,res) => {
+    
+})
+
+
 
 app.listen(port, () => {
     console.log(`API REST corriendo en http://localhost:${port}`)
